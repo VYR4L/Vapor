30c2d937b5cadf7ef3f042f3011526784a2508d0
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-04-01 16:43:13 -0300

    Update to Stock.c
    O código foi Splittado, agora, a função para cadastrar os jogos está fora da main.
    Foram adicionadas as funções para mostrar o estoque e excluir itens do estoque.

--- Stock.c
+++ Stock.c
@@ -1,6 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+
 struct library{
     char name[50];
     char MediaType[8];
@@ -13,77 +14,163 @@ struct library{
     char requirements[5][50];
 };
 
-int main(){
-    int n=0, i=0;
-    struct library *stock = NULL;
-    struct library *ptrS = NULL;
-
-    printf("Digite a quantity de produtos que deseja cadastrar: ");
-    scanf("%d", &n);
-
-    // Aloca memória para o array "stock"
-    stock = malloc(n * sizeof(struct library));
-
-    // Verifica se houve erro na alocação de memória
-    if(stock == NULL){
-        printf("Não foi possível alocar o estoque");
-        exit(1);
+void mostra_estoque(struct library *stock, int n){
+    for(int i = 0; i < n; i++){
+        printf("%d. Digite o nome do produto: %s\n",i+1 , stock[i].name);
+        printf("%d. Tipo de midia: %s\n",i+1 , stock[i].MediaType);
+        printf("%d. Quantidade: %d\n",i+1 , stock[i].quantity);
+        printf("%d. Preco: %f\n",i+1 , stock[i].price);
+        printf("%d. Tags: %s\n",i+1 , stock[i].tag);
+        printf("%d. MetaC: %f\n",i+1 , stock[i].metaC);
+        printf("%d. Desenvolvedora: %s\n",i+1 , stock[i].developer);
+        printf("%d. Data: %s\n",i+1 , stock[i].data);
+        for(int j=0; j<5; j++){
+            if(j == 0){
+                printf("%d. Sistema Operacional: %s\n",i+1 , stock[i].requirements[j]);
+            }
+            else if(j == 1){
+                printf("%d. Pprocessador: %s\n",i+1 , stock[i].requirements[j]);
+            }
+            else if(j == 2){
+                printf("%d. Memoria ram: %s\n",i+1 , stock[i].requirements[j]);
+            }
+            else if(j == 3){
+                printf("%d. Placa de video: %s\n",i+1 , stock[i].requirements[j]);
+            }
+            else if(j == 4){
+                printf("%d. Armazenameno Interno: %s\n",i+1 , stock[i].requirements[j]);
+            }
+        }
     }
-    // Realoca a memória do array "stock" com base na entrada do usuário
-    ptrS = realloc(stock, n * sizeof(struct library));
-    for(i = 0; i < n; i++){
-        printf("Digite o noame do produto: ");
-        scanf(" %c", &stock[i].name);
+}
+
+void remove_jogo(struct library **stock, int n){
+    int removeN=0;
+    printf("Digite o numero do jogo que deseja excluir do estoque: ");
+    scanf("%d", &removeN);
+
+    if (removeN < 1 || removeN > n) {
+        printf("Numero invalido.\n");
+        return;
+    }
+
+    for (int i = removeN-1; i < n-1; i++) {
+        stock[i] = stock[i+1];
+    }
+    n -= 1;
+    struct library *new_stock = realloc(*stock, n * sizeof(struct library *));
+    if (new_stock == NULL) {
+        printf("Nao foi possivel alocar");
+    }
+    *stock = new_stock;
+    printf("Jogo removido com sucesso.\n");
+}
+
+void cadastra_jogo(struct library **stock, int n){
+    *stock = realloc(*stock, n * sizeof(struct library *));
+    for(int i = 0; i < n; i++){
+        printf("Digite o nome do produto: ");
+        scanf(" %s", (*stock)[i].name);
         fflush(stdin);
 
-        printf("Digite o tipo de mídia: ");
-        scanf(" %c", &stock[i].MediaType);
+        printf("Digite o tipo de midia: ");
+        scanf(" %s", (*stock)[i].MediaType);
         fflush(stdin);
 
         printf("Digite a quantidade: ");
-        scanf("%d", &stock[i].quantity);
+        scanf("%d", &(*stock)[i].quantity);
         fflush(stdin);
 
-        printf("Digite o preço: ");
-        scanf("%f", &stock[i].price);
+        printf("Digite o preco: ");
+        scanf("%f", &(*stock)[i].price);
         fflush(stdin);
 
-        printf("Digite a tag: ");
-        scanf(" %c", &stock[i].tag);
+        printf("Digite as tags: ");
+        scanf(" %s", (*stock)[i].tag);
         fflush(stdin);
 
         printf("Digite a metaC: ");
-        scanf("%d", &stock[i].metaC);
+        scanf("%f", &(*stock)[i].metaC);
         fflush(stdin);
 
         printf("Digite a desenvolvedora: ");
-        scanf(" %c", &stock[i].developer);
+        scanf(" %s", (*stock)[i].developer);
         fflush(stdin);
 
         printf("Digite a data: ");
-        scanf(" %c", &stock[i].data);
+        scanf(" %s", (*stock)[i].data);
         fflush(stdin);
 
-        printf("Digite os requisitos: ");
+        printf("Digite os requisitos: \n");
         for(int j=0; j<5; j++){
+            char requisito[50];
             if(j == 0){
-                printf("Sistema Operacional:");
+                printf("Sistema Operacional: ");
             }
             else if(j == 1){
-                printf("processador:");
+                printf("Processador: ");
             }
             else if(j == 2){
-                printf("Memória ram:");
+                printf("Memoria ram: ");
             }
             else if(j == 3){
-                printf("Placa de vídeo:");
+                printf("Placa de video: ");
             }
             else if(j == 4){
-                printf("Armazenameno Interno:");
+                printf("Armazenamento Interno: ");
             }
-            scanf("%s", stock[i].requirements[j]);
+            fgets(requisito, 50, stdin);
+            sscanf(requisito, "%s", (*stock)[i].requirements[j]);
         }
-        fflush(stdin);
+    }
+}
+
+
+
+int main(){
+    int n=0, confirmacao;
+    struct library *stock = NULL;
+
+    printf("Digite a quantidade de produtos que deseja cadastrar: ");
+    scanf("%d", &n);
+
+    stock = malloc(n * sizeof(struct library *));
+    if(!stock){
+        printf("Nao foi possivel alocar o estoque");
+        exit(1);
+    }
+
+    printf("Digite a operacao que deseja realizar: ");
+    printf("(1-Cadastrar;\n2-Remover Jogo Cadastrado;\n3-Mostrar o Estoque;\n4-Sair.) ");
+    scanf("%d", &confirmacao);
+    while(confirmacao != 4) {
+        if (confirmacao == 1) {
+            cadastra_jogo(&stock, n);
+            printf("Digite a operacao que deseja realizar: ");
+            printf("(1-Cadastrar;\n2-Remover Jogo Cadastrado;\n3-Mostrar o Estoque;\n4-Sair.) ");
+            scanf("%d", &confirmacao);
+        }
+        else if (confirmacao == 2) {
+            mostra_estoque(stock, n);
+            remove_jogo(&stock, n);
+            printf("Digite a operacao que deseja realizar: ");
+            printf("(1-Cadastrar;\n2-Remover Jogo Cadastrado;\n3-Mostrar o Estoque;\n4-Sair.) ");
+            scanf("%d", &confirmacao);
+        }
+        else if (confirmacao == 3) {
+            mostra_estoque(stock, n);
+            printf("Digite a operacao que deseja realizar: ");
+            printf("(1-Cadastrar;\n2-Remover Jogo Cadastrado;\n3-Mostrar o Estoque;\n4-Sair.) ");
+            scanf("%d", &confirmacao);
+        }
+        else if(confirmacao == 4){
+            return 0;
+        }
+        else {
+            printf("Opcao invalida\n");
+            printf("Digite a operacao que deseja realizar: ");
+            scanf("%d", &confirmacao);
+        }
     }
     free(stock);
 }
