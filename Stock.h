f12df067be15e5c1b0c1a39d73f489ac920dbdba
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-04-01 18:04:12 -0300

    Update Stock.h

--- Stock.h
+++ Stock.h
@@ -3,6 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct library{
+    char name[50];
+    char MediaType[8];
+    int quantity;
+    float price;
+    char tag[100];
+    float metaC;
+    char developer[50];
+    char data[50];
+    char requirements[5][50];
+};
+
 void mostra_estoque(struct library *stock, int n);
 void remove_jogo(struct library **stock, int n);
 void cadastra_jogo(struct library **stock, int n);
