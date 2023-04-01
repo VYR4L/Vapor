45f21cfdf0ebbd188130cfcd377c104fe8677e6c
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-04-01 17:44:20 -0300

    Update User.c

--- User.c
+++ User.c
@@ -13,12 +13,11 @@ struct cadastro{
     char sexo[20];
 };
 
-int main(){
-    int n = 0, i = 0;
+int cadastraUsuario(){
+    int n = 0, i;
     int a = 1, b = 1;
     char confirm[20];
     struct cadastro *usuario = NULL;
-    struct cadastro *ptrC = NULL;
 
     printf("Digite a quantidade de Usuarios que deseja Cadastrar: ");
     scanf("%d", &n);
@@ -29,41 +28,40 @@ int main(){
         printf("Não foi possível alocar o estoque");
         exit(1);
     }
-    
-    ptrC = realloc(usuario, n * sizeof(struct cadastro));
+
     for(i = 0; i < n; i++){
         printf("Digite o nome da Pessoa: ");
-        scanf("%s", &usuario[i].name);
+        scanf("%s", usuario[i].name);
         fflush(stdin);
 
         printf("Digite o apelido que ira utilizar: ");
-        scanf("%s", &usuario[i].apelido);
+        scanf("%s", usuario[i].apelido);
         fflush(stdin);
-        
-	while(a != 0){
-        	printf("Cadastre a senha do Usuario: ");
-       	 	scanf("%s", &usuario[i].senha);
-        	fflush(stdin);
-        	printf("Confirme sua senha: ");
-        	scanf("%s", &confirm);
-        	a = strcmp(usuario[i].senha,confirm);
-	}
-      
-	 while(b != 0){
-        	printf("Digite o email do Usuario: ");
-       	 	scanf("%s", &usuario[i].email);
-        	fflush(stdin);
-        	printf("Confirme o Email: ");
-        	scanf("%s", &confirm);
-        	b = strcmp(usuario[i].email,confirm);
-	}
+
+        while(a != 0){
+            printf("Cadastre a senha do Usuario: ");
+            scanf("%s", usuario[i].senha);
+            fflush(stdin);
+            printf("Confirme sua senha: ");
+            scanf("%s", confirm);
+            a = strcmp(usuario[i].senha,confirm);
+        }
+
+        while(b != 0){
+            printf("Digite o email do Usuario: ");
+            scanf("%s", usuario[i].email);
+            fflush(stdin);
+            printf("Confirme o Email: ");
+            scanf("%s", confirm);
+            b = strcmp(usuario[i].email,confirm);
+        }
 
         printf("Digite o telefone do Usuario: ");
         scanf("%d", &usuario[i].tel);
         fflush(stdin);
 
         printf("Digite a cidade do Usuario: ");
-        scanf("%s", &usuario[i].local);
+        scanf("%s", usuario[i].local);
         fflush(stdin);
 
         printf("Digite a data de nascimento do Usuario: ");
@@ -71,10 +69,10 @@ int main(){
         fflush(stdin);
 
         printf("Digite o sexo do Usuario: ");
-        scanf("%s", &usuario[i].sexo);
+        scanf("%s", usuario[i].sexo);
         fflush(stdin);
-        
-	system("cls");
+
+        system("cls");
     }
     free(usuario);
 }
