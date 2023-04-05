f1dbe169f7893fcaaf34a35f0e9b8d023596698c
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-04-05 08:49:32 -0300

    .h do Farofa

---
+++ Relatorio.h
@@ -0,0 +1,11 @@
+#ifndef VAPOR_MAIN_RELATORIO_H
+#define VAPOR_MAIN_RELATORIO_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+struct relatorio{
+    float valorcompra;
+    char funcionario[100];
+};
+void relato();
+#endif //VAPOR_MAIN_RELATORIO_H
