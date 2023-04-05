f975caa7b034d4d65be79fb95ef56b59c67935cc
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-04-05 08:53:40 -0300

    Update User.h

--- User.h
+++ User.h
@@ -5,6 +5,19 @@
 #include<stdio.h>
 #include<string.h>
 
-int cadastraUsuario();
+int m = 0;
+struct cadastro *usuario = NULL;
+
+struct cadastro{
+    char name[100];
+    char apelido[50];
+    char senha[20];
+    char email[50];
+    int tel;
+    char local[50];
+    int nasci;
+    char sexo[20];
+};
+void cadastraUsuario();
 
 #endif
